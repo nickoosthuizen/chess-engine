3a109a35c9dbb2e384169d2658723f245ac26e79
Nick Oosthuizen <nickoosthuizen@g.ucla.edu>
2021-07-28 22:45:20 -0500

    Added get functions for the different bitboard types

--- src/board.h
+++ src/board.h
@@ -3,16 +3,39 @@
 
 #include <cstdint>
 
-struct board {
-  uint64_t occupied;
-  uint64_t empty;
-  uint64_t pieces[2];
-  uint64_t pawns[2];
-  uint64_t knights[2];
-  uint64_t bishops[2];
-  uint64_t rooks[2];
-  uint64_t queens[2];
-  uint64_t king[2];
+enum boardType {
+  white,
+  black,
+  pawns,
+  knights,
+  rooks,
+  bishops,
+  queens,
+  kings
 };
+
+class board {
+  public:
+    uint64_t getAllPieces();
+    uint64_t getEmptySquares();
+    uint64_t getWhitePieces();
+    uint64_t getWhitePawns();
+    uint64_t getWhiteKnights();
+    uint64_t getWhiteRooks();
+    uint64_t getWhiteBishops();
+    uint64_t getWhiteQueens();
+    uint64_t getWhiteKing();
+    uint64_t getBlackPieces();
+    uint64_t getBlackPawns();
+    uint64_t getBlackKnights();
+    uint64_t getBlackRooks();
+    uint64_t getBlackBishops();
+    uint64_t getBlackQueens();
+    uint64_t getBlackKing();
+
+  private:
+    uint64_t m_boards[8];
+};
+
 
 #endif
